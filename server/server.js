be13be358cc48d7648de8f2a6fbff2d860e13841
Saravana <100523948+Saravana-Pon-Balan@users.noreply.github.com>
2024-04-04 16:50:14 +0530

    socket io added

--- server/server.js
+++ server/server.js
@@ -1,40 +1,43 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server,{
-  cors:{
-    origin:['http://localhost:3000'],
-  }
-});
+// server.js
+import express from "express";
 import connectDB from "./dbConnection/index.js";
 import routes from "./routes/index.js";
 import configureExpressApp from "./config/index.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+configureExpressApp(app);
+
+const httpserver = createServer(app);
+const io = new Server(httpserver,{
+  cors: {
+    origin: "*", // Allow all origins for now, adjust as needed
+    methods: ["GET", "POST"] // Allow only GET and POST requests
+  }
+});
 
 const PORT = 3001;
 
-configureExpressApp(app);
-routes(app);
+io.on("connection", (socket) => {
+  socket.on('user', function (data) {
+    socket.join(data.room);
+    console.log(data)
+    socket.broadcast.to(data.room).emit("msg",data.msgReqData)
+    console.log("sent");
 
-io.on("connect", (socket) => {  
-  socket.on('send-message',(messages,channelId)=>{
-    console.log(messages);
-    socket.to(channelId).emit('receive-message',messages)
-  })
-  socket.on('join-channel',channelId=>{
-    socket.join(channelId);
-    console.log(`joined ${channelId}`);
-  })
-
-
-  socket.on('disconnect', () => {
-  console.log('Client disconnected');
   });
-  });
-
-connectDB().then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server started on Port ${PORT}`);
-  });
-}).catch((error) => {
-  console.error(`Unable to start the server`, error);
 });
+
+
+const startServer = () => {
+  Promise.all([connectDB()])
+    .then(() => {
+      httpserver.listen(PORT);
+      console.log(`Server started on Port ${PORT}`);
+      routes(app);
+    }).catch((error) => console.error(`Unable to start the server`, error));
+};
+
+startServer();
